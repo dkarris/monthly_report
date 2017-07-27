028d3125dd4e2f82c517c2cde23480d506862e65
Dimitri Karlov <dkarlov2005@gmail.com>
2017-07-26 23:32:06 -0400

    data loading complete

--- js/report_generator.js
+++ js/report_generator.js
@@ -1,9 +1,11 @@
 //  variables defined at global level
 
+var parsedData = [];
 
 var ViewModel = {
     self : this,
-    parsedJson: ko.observable(),
+    parsedErrors: ko.observable(),
+    parsedData: ko.observable()
     // convertRecords: ko.computed(function() {
     //      return this.parsedJson.data
     // },this)
@@ -36,15 +38,40 @@ function parseCsv() {
     Papa.parse(file, config);
 };
 function processJSON(result,file) {
-    ViewModel.parsedJson(result);
-    x = result;
-    console.log(x);
+    // also do a lenght - 1 : erase the last record => which triggers
+    // while doing copying to new array rename headers because in excel there are spaces
+    // so either redo excel or correct it here
+    // second option better since don't change anything in the original and not  break links
+    // to HFM
+    console.log(result.data.length-2);
+    for (var i=0; i<result.data.length-1;i++) {
+        var currentRow = result.data[i];
+        var parsedRow = {
+            'mtd_qtd_ytd' : currentRow['MTD_QTD_YTD'],
+            'country'     : currentRow['country'],
+            'region'      : currentRow['region'],
+            'month'       : currentRow['month'],
+            'actual'      : currentRow['Actual'],
+            'actual_Fbp'  : currentRow['Actual@FBP'],
+            'actual_Ju'   : currentRow['Actual@JU'],
+            'actual_Pyrate':currentRow['Actual@PYrate'],
+            'fbp'         : currentRow['FBP'],
+            'ju'          : currentRow['JU'],
+            'ju_dec'      : currentRow['JU_DEC'],
+            'ju_sep'      : currentRow['JU_Sep'],
+            'py_actualFx' : currentRow['PY@Actual FX'],
+            'py_fbp16'    : currentRow['PY@FBP16'],
+            'py_fbp17'    : currentRow['PY@FBP17'],
+            'py_dec'      : currentRow['py_dec']
+        };
+        parsedData.push(parsedRow);
+    }
+    ViewModel.parsedErrors(result.errors);
+    ViewModel.parsedData(parsedData);
+    console.log(parsedData);
 }
 function errorHandler (error,file) {
     alert (error);
     console.log(error);
 }
-ViewModel.convertErrors = ko.computed(function() {
-    return ViewModel.parsedJson().data
-    })
 
