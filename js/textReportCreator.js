f2eac10f8a7d69ae53fa6cafa3f46e589f358c2e
Dimitri Karlov <dkarlov2005@gmail.com>
2017-11-27 23:01:39 -0500

    Nov27V2

---
+++ js/textReportCreator.js
@@ -0,0 +1,22 @@
+
+//field maybe py/fbp/ju/nu 
+// comparison: abs/pct
+
+//global consts for text report
+
+
+
+function textReport(field,comparison) {
+    
+    // check possible fields
+    
+    var fieldValues = ['py','fbp','ju','nu'];
+    if (fieldValues.indexOf(field) == 0) {return null;}
+
+    // py case
+
+    if (field == 'py') {
+
+    }
+
+}
