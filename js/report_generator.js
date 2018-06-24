9db2c11a95278dcc37c3a24a78814c707cc96503
Dimitri Karlov <dkarlov2005@gmail.com>
2018-06-24 13:02:57 -0400

    first prototype report

--- js/report_generator.js
+++ js/report_generator.js
@@ -407,18 +407,320 @@ function processJSON(result,file) {
     $("#csv_load").dataTable();
 }
 
-function errorHandler (error,file) {
-    alert (error);
-    console.log(error);
+
+// **************************************************/
+// Numbers to text report functions go here
+//
+// **************************************************/
+
+//Change String prototype to include format 
+//https://stackoverflow.com/questions/610406/javascript-equivalent-to-printf-string-format
+
+// "foo {0},{1}".format('bar1','bar2')
+String.prototype.format = function (){
+    var args = [];
+    // now check each argument for "//,,//" occurence. If it is then it means caller passed concatenated 
+    // array of parameters and we need to split it.
+    // each argument must be string or array of strings
+    Array.prototype.slice.call(arguments).forEach(function (element){
+        if (element.indexOf('//,,//') > 0) {
+            element.split('//,,//').forEach(function(substring) {args.push(substring);})
+        }
+        else {
+            args.push(element);
+        }
+    });
+    return this.replace(/\{\{|\}\}|\{(\d+)\}/g, function (curlyBrack, index) {
+        return ((curlyBrack == "{{") ? "{" : ((curlyBrack == "}}") ? "}" : args[index]));
+    });
+};
+
+function generateHeader(node){
+// full D3 object - reads to level with WW and generates Summary header
+    reportString = "Summary \n WW Sales for the {0} quarter of 2018 were {1}, which is an {2} of {3} or {4} vs {5}. "
+     +"Excluding the impact of the Codman Neurosurgery (CNS) divestiture, WW Ortho growth was {6}. Adjusting for the negative" + 
+     "impact of the CNS divestiture and the OUS selling days variance, WW growth was {7} \n."
+// generate substitues for the string
+    var quarter = 'second';
+    var salesCY$ = node.actual;
+    var salesPY$ = node.pyAtActual;
+    var opsResult$ = salesCY$ - salesPY$;
+    var opsResultText = opsResult$ <0 ? "operational decline" : "operational growth";
+    var opsResultPCT = (((salesCY$/salesPY$-1)*100).toFixed(2))+"%";
+    var quarterPY = "2Q17";
+    return reportString.format(quarter,salesCY$.toFixed(2),opsResultText, opsResult$.toFixed(2),opsResultPCT,quarterPY); 
 }
 
-// Generate line of report per each node
-function generateReport(platform_node, params) {
+function generateReportByPlatform(platformNode) {
     
+    // WW Trauma (+4.5%; +5.2% ADS) "driven" by
+    var reportMainString = " * WW {0} ({1};{2}) {3} by";
+    var platformName = platformNode.key;
+    var platformCY$ = platformNode.actual;
+    var platformPY$ = platformNode.pyAtActual;
+    var platformOpsResult$ = platformCY$ - platformPY$;
+    var platformOpsResultText = platformOpsResult$ < 0 ? " driven" : " driven"
+    var platformOpsResultPCT = (((platformCY$/platformPY$-1)*100).toFixed(2))+"%";
+    var platformADSResultPCT = "WW Platform ADS calc here"; //To do add ADS calc and change later to accomodate for str format
+    reportMainString = reportMainString.format(platformName, platformOpsResult$.toFixed(2), platformADSResultPCT, platformOpsResultText);
+    //Now execute report by region and concatenate the report mainstring
+    
+    var platformRegionReports = [];
+    var platformGrowth = platformOpsResult$ < 0 ? false: true; 
+    var platformObject = {}; //object to store platform parameters such us growth within certain areas/regions display order, etc.
+    var regionsFactorsAll = '';
+    var regionsFactorsAll = '';
+    platformNode.values.forEach(function (region) {
+        regionsFactorsAll = regionsFactorsAll + subReportPlatformRegion(platformName,region);
+    });
+    return reportMainString + regionsFactorsAll + '\n';
 }
 
-function sortObjects (node,sortKey) {
-    return node.values.sort(function(element1, element2){
-        return element1[sortKey] - element2[sortKey]
+function subReportPlatformRegion(platformName,region) {
+// called from generateReportByPlatform and ....
+// returns summary by regions within defined platform
+    var regionName = region.key;
+    regionName = (regionName == 'US') ? "the US" : regionName;
+    var regionCY$ = region.actual;
+    var regionPY$ = region.pyAtActual;
+    var regionOpsResult$ = regionCY$ - regionPY$;
+    var regionOpsResultText = regionOpsResult$ < 0 ? "driven by" : "fueled by"
+    var regionOpsResultPCT = (((regionCY$/regionPY$-1)*100).toFixed(2))+"%";
+    var regionADSResultPCT = "Region ADS calc here"; //To do add ADS calc and change later to accomodate for str format
+    var regionGrowth = regionOpsResult$ < 0 ? false : true;
+    
+    ///temp object for development. SHould be linked to excel in prod.
+
+    var platformRegionFactor = {
+        "Jointsthe US" : ["Joints  US factor1", "JointsUS factor2"],
+        "JointsEMEA" : ["Joints EMEA factor 1"],
+        "JointsASPAC" : ["Joints ASPAC factor1"],
+        "JointsLATAM" : ["JointsLAtam factor1", "JointsLAtam factor2", "JointsLAtam factor3"]
+    }
+
+     
+    //drill down by country
+    
+    // •	.... the U.S. (+3.8%; +3.8% ADS) fueled by market volume growth 
+    // and continued strong sales of TFNA. ASPAC (+10.0.%; +12.3% ADS) growth was driven by China’s return to normal inventory, volume growth in MEA, 
+    // and NPI sales in Japan. EMEA (+4.4%; +5.4% ADS) growth was the result of competitor account conversion in UK and Ireland. LATAM (+1.9%; +5.2% ADS) 
+    // growth was driven by new government bid in Costa Rica. 
+    
+    var subReportPlatformRegionString = platformRegionFactor[(platformName+regionName)];
+    
+    
+    var mergedPlatformRegionFactors = '';
+
+
+    if (subReportPlatformRegionString) { //if exists key in factors then form factors string
+    
+        // if array > 1 this means that we have at least  2 factors and need to merge them with "and"
+        // if not the last element then add factor text +','. 
+        // if last item delete last ',' - slice(0,-1) also add "AND" + factor
+        if (subReportPlatformRegionString.length>1) {
+               subReportPlatformRegionString.forEach(function (factor, index, array){
+                    (array.length - index == 1 ) ? mergedPlatformRegionFactors = mergedPlatformRegionFactors.slice(0,-1) + ' and '
+                     + factor + '.' : mergedPlatformRegionFactors = mergedPlatformRegionFactors + ' ' + factor + ',';
+        });
+        } else if (subReportPlatformRegionString.length == 1) { 
+            mergedPlatformRegionFactors =  subReportPlatformRegionString[0];
+        } else {
+            // do nothing if no region factor. Will return empty string
+        };
+    }    // create output string and return in
+    var outputRegion = regionName + ' (' + regionOpsResultPCT + '; ' + regionADSResultPCT + ') ' + regionOpsResultText + ' ' + mergedPlatformRegionFactors + '.';
+    return outputRegion;
+}
+
+
+// Markets:
+// (the below market commentary excludes the impact of the Codman Neurosurgery divestiture impact)
+// Q1 operational decline of - 1.9 % (excluding Codman) was driven by negative growth in the U.S.of - 2.4 % (-2.4 % ADS),
+//  EMEA decline of - 2.4 % (-1.5 % ADS), LATAM - 4.3 % (-1.2 % ADS) and Canada of - 2.1 % (-0.5 % ADS), 
+//partially offset by growth in ASPAC of + 2.0 % (+4.2 % ADS).  
+function generateMarketsHeader(D3NestObject)
+ {
+    
+    var marketsObject = D3NestObject; // create copy of object and work with it 
+    var quarter = 'Q2';
+    var salesCY$ = marketsObject.actual;
+    var salesPY$ = marketsObject.pyAtActual;
+    var opsResult$ = salesCY$ - salesPY$;
+    var opsResultText = opsResult$ < 0 ? "operational decline" : "operational growth";
+    var opsResultGrowth = opsResult$ <0 ? "negative growth in" : "positive growth in";
+    var opsResultPCT = (((salesCY$ / salesPY$ - 1) * 100).toFixed(2)) + "%";
+    var reportHeaderString = '{0} {1} of {2} (excluding Codman) was driven by {3} '.format(quarter,opsResultText,opsResultPCT, opsResultGrowth);
+    
+    // in the U.S.of - 2.4 % (-2.4 % ADS), EMEA decline of - 2.4 % (-1.5 % ADS), 
+    // LATAM - 4.3 % (-1.2 % ADS) and Canada of - 2.1 % (-0.5 % ADS), 
+    // partially offset by growth in ASPAC of + 2.0 % (+4.2 % ADS)
+
+    // generate string based on opsResult
+    // partyallyOffset flag true when need to add partially offset for the first time 
+    var partiallyOffset = false; 
+    var marketStrings = ''
+    if (opsResult$ < 0 ) {
+        marketsObject.values.sort(function (regionA, regionB) {
+            return Number(regionA['vs_PY_$']) - Number(regionB['vs_PY_$'])
+        });
+        marketStrings = marketsObject.values.reduce(function (marketStrings, region) {
+            var marketString = region.key + ' (' + region['vs_PY_Pct_ops'] + '%;(ADS will be here),';
+            if (Number(region['vs_PY_Pct_ops'])>0 && !(partiallyOffset)) {
+                marketString = 'partially offset by ' + marketString;
+                partiallyOffset = true;
+            };
+            return marketStrings + marketString;
+        },'');
+    } else { // if ops growth ==0 still treat as positive otherwise need to build another logic
+        marketsObject.values.sort(function (regionA, regionB) {
+            return Number(regionB['vs_PY_$']) - Number(regionA['vs_PY_$']) 
+        });
+        marketStrings = marketsObject.values.reduce(function (marketStrings, region) {
+            var marketString = region.key + ' (' + region['vs_PY_Pct_ops'] + '%;(ADS will be here),';
+            if (Number(region['vs_PY_Pct_ops']) < 0 && !(partiallyOffset)) {
+                marketString = 'partially offset by ' + marketString;
+                partiallyOffset = true;
+            };
+            return marketStrings + marketString;
+        }, '');
+    }
+    return reportHeaderString + marketStrings;
+}
+
+
+function generateMarketsUSPiece(USRegionNode) {
+  //  U.S.decline of - 3.8 % was driven primarily by Spine(-10.8 %) and Knees(-7.1 %), 
+  // partially offset by Trauma(+3.8 %) and Other Recon(+9.4 %).
+  // There was no official U.S.selling day difference between 1Q18 and 1Q17.However, there was an impact to elective surgeries in 1Q18 
+  // related to the timing of Easter / Good Friday and New Year's Day.  
+  // We estimate the impact to be equivalent to 1 selling day, or $14.2MM. Excluding this holiday impact, U.S. decline would have been -2.8%.    
+    var salesCY$ = USRegionNode.actual;
+    var salesPY$ = USRegionNode.pyAtActual;
+    var opsResult$ = salesCY$ - salesPY$;
+    var opsResultPCT = (((salesCY$ / salesPY$ - 1) * 100).toFixed(2)) + "%";
+    var opsResultGrowth = opsResult$ < 0 ? "decline of" : "growth of";
+    var USreportString = 'U.S. {0} of {1} was driven primary by '.format(opsResultPCT, opsResultGrowth);
+    
+        
+    var partiallyOffset = false;
+    var platformStrings = '';
+    if (opsResult$ < 0) {
+        USRegionNode.values[0].values.sort(function (platformA, platformB) {
+            return Number(platformA['vs_PY_$']) - Number(platformB['vs_PY_$']);
+        });
+        platformStrings = USRegionNode.values[0].values.reduce(function (platformStrings, platform) {
+            var platformString = platform.key + ' (' + platform['vs_PY_Pct_ops'] + '%;(ADS will be here),';
+            if (Number(platform['vs_PY_Pct_ops']) > 0 && !(partiallyOffset)) {
+                platformString = 'partially offset by ' + platformString;
+                partiallyOffset = true;
+            };
+            return platformStrings + platformString;
+        }, '');
+    } else { // if ops growth ==0 still treat as positive otherwise need to build another logic
+        USRegionNode.values[0].values.sort(function (platformA, platformB) {
+            return Number(platformB['vs_PY_$']) - Number(platformA['vs_PY_$']);
+        });
+        platformStrings = USRegionNode.values[0].values.reduce(function (platformStrings, platform) {
+            var platformString = platform.key + ' (' + platform['vs_PY_Pct_ops'] + '%;(ADS will be here),';
+            if (Number(platform['vs_PY_Pct_ops']) < 0 && !(partiallyOffset)) {
+                platformString = 'partially offset by ' + platformString;
+                partiallyOffset = true;
+            };
+            return platformStrings + platformString;
+        }, '');
+    }
+    return USreportString + platformStrings + 'ADD US EVENTS HERE';
+}
+
+function generateMarketsOUSPiece(OUSRegionArray) {
+   // From a total OUS standpoint, operational net trade sales decline of - 1.1 % (excluding Codman) 
+   // was driven by Australia(-14.3 %) and UK(-12.7 %), partially offset by growth in China(+16.3 %).
+   // Excluding the net impact of the selling days, operational growth was + 0.4 %.  
+    
+    var salesCY$ = OUSRegionArray.reduce(function (salesCY, region) {
+        return salesCY + Number(region['actual']);        
+    },0)
+    var salesPY$ = OUSRegionArray.reduce(function (salesPY, region){
+        return salesPY + Number(region['pyAtActual']);
+    },0)
+
+    var opsResult$ = salesCY$ - salesPY$;
+    var opsResultPCT = (((salesCY$ / salesPY$ - 1) * 100).toFixed(2)) + "%";
+    var opsResultGrowth = opsResult$ < 0 ? "decline of" : "growth of";
+    var offsetType = opsResult$ < 0 ? "partially offset by growth in" : "partially offset by decline in";
+
+    var OUSHeaderString = 'From a total OUS standpoint, operational net trade sales {0} of {1} (excluding Codman)\
+    was driven by'.format(opsResultGrowth, opsResultPCT);
+
+    var allCountries = []; //Combine all regions OUS to one array for sorting in the next steps:
+    OUSRegionArray.forEach(function (region) {
+        region.values.forEach(function (country) {
+            allCountries.push(country);
+        })
     })
+
+    // sort All countries depending on whether we decline or grow and select bottow x + top countries for report
+    
+    if (opsResult$ < 0) {
+        OUSRegionArray.sort(function (countryA, countryB) {
+            return Number(countryA['vs_PY_$']) - Number(countryB['vs_PY_$']);
+        })
+    }   else {
+        OUSRegionArray.sort(function (countryA, countryB) {
+            return Number(countryB['vs_PY_$']) - Number(countryA['vs_PY_$']);
+        })
+    }
+
+    const bottomCountriesN = topCountriesN = 3; 
+    var bottomCountries = allCountries.slice(0,bottomCountriesN)
+    var topCountries = allCountries.slice(-topCountriesN)
+    var bottomCountriesString = bottomCountries.reduce(function (bottomCountriesString, country) {
+        var countryString = country.key + ' (' + country['vs_PY_Pct_ops'] + '%),';
+        return bottomCountriesString + countryString;        
+    },'')
+    var topCountriesString = topCountries.reduce(function (topCountriesString, country) {
+        var countryString = country.key + ' (' + country['vs_PY_Pct_ops'] + '%),';
+        return topCountriesString + countryString;
+    }, '')
+
+    OUSHeaderString = OUSHeaderString + bottomCountriesString +';partially ofset by ' + topCountriesString+'\n';
+
+    // * UK(-$8.6MM; -12.7 %) was impacted negatively in Hips, Knees and Spine 
+    // by continued NHS funding issues and budgetary constraints    
+
+    var countryString = bottomCountries.reduce(function (countryString, country) {
+        var countryParagraph = '* ' + country.key + ' (' + country['vs_PY_$'] + '); ' +
+         country['vs_PY_Pct_ops'] +'%) was impacted ......INSERT REASON HERE....\n'
+        return countryString + countryParagraph;
+    },'')
+
+    OUSHeaderString = OUSHeaderString + countryString;
+    return OUSHeaderString;
+
+}
+
+
+// main function runs headers generation and concatenates each platfrom/region report by calling each element 
+// within array.reduce()
+function generateReportPlatform() {
+    var totalPlatformReport = '';
+    return (generateHeader(tempArray) + tempArray.values.reduce(function(totalPlatformReport, region){ 
+        return totalPlatformReport + generateReportByPlatform(region);
+    },''));
+}
+
+function generateReportMarket() {
+    // var totalMarketReport = '';
+    // Separate for US and OUS piece
+    var OUSRegionArray = [];
+    for (var x = 0; x<tempArray.values.length; x++) {
+        if (tempArray.values[x].key == 'US') {
+            var USNode = tempArray.values[x];
+        } else {
+            OUSRegionArray.push(tempArray.values[x]);
+        }
+    }
+
+    // Run US and OUS pieces
+    return generateMarketsHeader(tempArray) + generateMarketsUSPiece(USNode) + generateMarketsOUSPiece (OUSRegionArray);
 }
